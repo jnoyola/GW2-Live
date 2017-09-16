cfc45c99649ff2d13000c35fdd3f091e1732cb44
Jonathan Noyola <jonoyola@microsoft.com>
2017-09-16 10:53:08 -0700

    added tests

---
+++ Test/Player/NodeTests.cs
@@ -0,0 +1,17 @@
+﻿using GW2_Live.Player;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Player
+{
+    [TestClass]
+    public class NodeTests
+    {
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void Insert_Should_RespectPriority()
+        {
+            Node
+        }
+    }
+}
+
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void TestMethod1()
+        {
+        }
+    }
+}
